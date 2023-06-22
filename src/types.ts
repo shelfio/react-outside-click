aeb00acb05b1f2f4cec4311a4954591f118af717
Andrrii <butsaandrii@gmail.com>
2023-06-22 17:40:09 +0300

    feat: REACT-477 added types.ts

---
+++ src/types.ts
@@ -0,0 +1,14 @@
+import type {ReactNode, RefObject} from 'react';
+
+type CommonProps = {
+  onOutsideClick: (event: MouseEvent) => void;
+  disabled?: boolean;
+  mouseEvent?: 'mousedown' | 'mouseup';
+  options?: boolean | AddEventListenerOptions;
+};
+
+export type OutsideClickWrapperProps = {children: ReactNode} & CommonProps;
+
+export type UseClickOutsideHandlerProps<T extends HTMLElement = HTMLElement> = {
+  ref: RefObject<T>;
+} & CommonProps;
