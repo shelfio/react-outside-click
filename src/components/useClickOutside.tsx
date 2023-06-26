c5725c54755ee702fccb690158a1893cfe1201e7
Andrrii <butsaandrii@gmail.com>
2023-06-26 12:37:50 +0300

    refactor: REACT-477 refactored useClickOutside.tsx

--- src/components/useClickOutside.tsx
+++ src/components/useClickOutside.tsx
@@ -1,13 +1,12 @@
 import {useEffect, useRef} from 'react';
-import type {UseClickOutsideHandlerProps} from '../types';
+import type {ClickOutsideHandler, Options, Ref} from '../types';
 
-export const useClickOutside = ({
-  ref,
-  onOutsideClick,
-  disabled,
-  mouseEvent = 'mousedown',
-  options = true,
-}: UseClickOutsideHandlerProps) => {
+export const useClickOutside = (
+  ref: Ref,
+  onOutsideClick: ClickOutsideHandler,
+  options?: Options
+) => {
+  const {mouseEvent = 'mousedown', disabled, listenerOptions = true} = options || {};
   const savedHandler = useRef(onOutsideClick);
 
   useEffect(() => {
@@ -25,11 +24,11 @@ export const useClickOutside = ({
       // `useCapture` for `options` is set to true by default to handle the capture phase of the event.
       // It ensures that the click outside event will be captured before any click events on the descendants,
       // even if `stopPropagation` is used.
-      document.addEventListener(mouseEvent, handleClickOutside, options);
+      document.addEventListener(mouseEvent, handleClickOutside, listenerOptions);
     }
 
     return () => {
-      document.removeEventListener(mouseEvent, handleClickOutside, options);
+      document.removeEventListener(mouseEvent, handleClickOutside, listenerOptions);
     };
-  }, [mouseEvent, disabled, options, ref]);
+  }, [mouseEvent, disabled, listenerOptions, ref]);
 };
