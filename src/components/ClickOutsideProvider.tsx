83d0eef9da5e98b7920af895a128999e5a7ae6c2
Andrrii <butsaandrii@gmail.com>
2023-06-26 12:38:33 +0300

    refactor: REACT-477 refactored ClickOutsideProvider.tsx

--- src/components/ClickOutsideProvider.tsx
+++ src/components/ClickOutsideProvider.tsx
@@ -2,9 +2,13 @@ import {useRef} from 'react';
 import type {ClickOutsideProviderProps} from '../types';
 import {useClickOutside} from './useClickOutside';
 
-export const ClickOutsideProvider = ({children, ...props}: ClickOutsideProviderProps) => {
+export const ClickOutsideProvider = ({
+  children,
+  onOutsideClick,
+  ...options
+}: ClickOutsideProviderProps) => {
   const ref = useRef<HTMLDivElement>(null);
-  useClickOutside({ref, ...props});
+  useClickOutside(ref, onOutsideClick, options);
 
   return <div ref={ref}>{children}</div>;
 };
