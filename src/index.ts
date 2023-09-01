034550d1066b091686851610f2c5945e0ace1920
Andrrii <butsaandrii@gmail.com>
2023-09-01 16:53:57 +0300

    feat: add proper imports part 2

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import {ClickOutsideProvider} from './components/ClickOutsideProvider.tsx';
-import {useClickOutside} from './components/useClickOutside.tsx';
+import {ClickOutsideProvider} from './components/ClickOutsideProvider.js';
+import {useClickOutside} from './components/useClickOutside.js';
 
 export {ClickOutsideProvider, useClickOutside};
