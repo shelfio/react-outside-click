71fce46cd4a9ff89bbd61b6970c796bdd4e3fcb6
Yaroslav Chapelskyi <chapelskyi.slavik@gmail.com>
2023-11-01 00:43:35 +0200

    test: proper testing lib setup

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 const config = {
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  setupFilesAfterEnv: ['./jest.setup.ts'],
   preset: 'ts-jest/presets/default-esm',
   resolver: 'ts-jest-resolver',
   coverageThreshold: {
